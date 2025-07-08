8590217bfabe4a6e3b0fb144a7f63ddda2ce263a
João Gomes <joaogomes@significa.co>
2025-07-08 14:26:37 +0100

    3: High-Level Transformations

---
+++ servy/lib/servy/handler.ex
@@ -0,0 +1,42 @@
+defmodule Servy.Handler do
+  def handle(request) do
+    # conv = parse(request)
+    # conv = route(conv)
+    # format_response(conv)
+
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
+
+  def parse(request) do
+    conv = %{method: 'GET', path: '/wild', resp_body: ""}
+  end
+
+  def route(conv) do
+    conv = %{method: 'GET', path: '/wild', resp_body: "Significa, Egg, FCPorto"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Significa, Egg, FCPorto
+    """
+  end
+end
+
+request = """
+GET /learnix HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
